c3b7cab49b354b181524a17b2cfad3f62b031aca
shakyasamin <saminshakya27@gmail.com>
2023-11-06 10:25:55 +0545

    created funtion to checked loggedin and verify token

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import { verifyToken } from "@/pages/Login/feature/tokenSlice";
+
 export default function useAuth() {
-    const dispatch = useappdisp
+  const dispatch = useAppDispatch();
+  const { status } = useAppSelector((state) => state.token);
+
+  const checkLoggedIn = async () => {
+    await dispatch(verifyToken());
+  };
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
+  return { isLoggedIn: status };
 }
